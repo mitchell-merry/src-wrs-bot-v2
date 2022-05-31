b9bc2e43ad884dae1def146b9675878d9a168b2e
Mitchell Merry <mitchellmerry145@gmail.com>
2022-05-31 20:23:28 +1000

    Specify JoinColumn names for TrackedLeaderboard

--- src/db/models/TrackedLeaderboard.model.ts
+++ src/db/models/TrackedLeaderboard.model.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { Guild, Leaderboard } from "./";
 
 @Entity()
@@ -10,10 +10,12 @@ export class TrackedLeaderboard {
 
 	/** The guild the leaderboard is being tracked in. */
 	@ManyToOne(() => Guild, guild => guild.trackedLeaderboards)
+	@JoinColumn({ name: 'guild_id' })
 	guild!: Guild;
 
 	/** The leaderboard being tracked. */
 	@ManyToOne(() => Leaderboard, leaderboard => leaderboard.trackedLeaderboards)
+	@JoinColumn({ name: 'lb_id' })
 	leaderboard!: Leaderboard;
 
 	/** The ID of the role associated with the leaderboard in the discord. */
