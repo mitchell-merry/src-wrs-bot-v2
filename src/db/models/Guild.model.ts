0efca0598127ae091879988c9bf25b196fa290dc
Mitchell Merry <mitchellmerry145@gmail.com>
2022-05-31 20:00:00 +1000

    Add docs to Guild entity properties.

--- src/db/models/Guild.model.ts
+++ src/db/models/Guild.model.ts
@@ -3,12 +3,15 @@ import { Column, Entity, PrimaryColumn } from "typeorm";
 @Entity()
 export class Guild {
 
+	/** The discord guild's ID. */
 	@PrimaryColumn()
 	guild_id!: string;
 
+	/** The default colour to give to newly created roles. */
 	@Column({ default: "#FEE75C" })
 	role_default_color!: string;
 
+	/** The ID of the role to place new roles above. */
 	@Column()
 	above_role_id!: string;
 }
