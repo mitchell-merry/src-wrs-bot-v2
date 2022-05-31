8069776c6fdca8532e904ab937e801e0aaa9c82f
Mitchell Merry <mitchellmerry145@gmail.com>
2022-05-31 23:43:58 +1000

    Add default for above_role_id and change double quotes to single

--- src/db/models/Guild.model.ts
+++ src/db/models/Guild.model.ts
@@ -1,7 +1,7 @@
-import { Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
-import { TrackedLeaderboard } from "./";
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { TrackedLeaderboard } from './';
 
-@Entity({ name: "guild" })
+@Entity({ name: 'guild' })
 export class GuildEntity {
 
 	/** The discord guild's ID. */
@@ -9,11 +9,11 @@ export class GuildEntity {
 	guild_id!: string;
 
 	/** The default colour to give to newly created roles. */
-	@Column({ default: "#FEE75C" })
+	@Column({ default: '#FEE75C' })
 	role_default_color!: string;
 
 	/** The ID of the role to place new roles above. */
-	@Column()
+	@Column({ default: '' })
 	above_role_id!: string;
 
 	/** The leaderboards this guild is tracking. */
