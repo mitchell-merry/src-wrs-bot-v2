31d3bdedf5b8f37c534888273e13717ec20370bb
Mitchell Merry <mitchellmerry145@gmail.com>
2022-05-31 23:30:05 +1000

    Couple comments

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,13 @@ const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] });
 client.on('ready', async () => {
 	if(!client.user) throw new Error(`Null client.user? ${client}`);
 	
+	// Log guilds bot is in on startup
 	console.log(`Logged in as ${client.user.tag} and in the following Guilds: [${client.guilds.cache.size}]`);
 	client.guilds.cache.forEach(g => {
 		console.log(`[${g.id}] ${g.available ? g.name : "UNAVAILABLE"} `)
 	});
 
+	// Initialise database
 	await DB.initialize()
 		.then(() => console.log("Data Source has been initialized!"))
 		.catch(err => console.error("Error during Data Source initialization", err));
