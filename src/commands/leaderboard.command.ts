94cd5d74e7579acdd91d64cfe5609a7f1b3101f9
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-02 09:49:42 +1000

    add setup for leaderboard subcommands

--- src/commands/leaderboard.command.ts
+++ src/commands/leaderboard.command.ts
@@ -3,7 +3,17 @@ import { CommandInteraction } from "discord.js";
 
 export const data = new SlashCommandBuilder()
 	.setName('leaderboard')
-	.setDescription('Manage the leaderboards for this guild.');
+	.setDescription('Manage the leaderboards for this guild.')
+	.addSubcommand(sc => sc
+		.setName('add')
+		.setDescription('Add a leaderboard to this guild.')
+		.addStringOption(o => o.setName('game').setDescription('The game abbreviation of the leaderboard to add.').setRequired(true)))
+	.addSubcommand(sc => sc
+		.setName('remove')
+		.setDescription('Remove a leaderboard from this guild.'))
+	.addSubcommand(sc => sc
+		.setName('list')
+		.setDescription('Lists all leaderboards tracked in this guild.'));
 
 export const perms = {
 	'add': 'mods',
@@ -11,6 +21,29 @@ export const perms = {
 	'list': 'all'
 };
 
-export const execute = async (interaction: CommandInteraction) => {        
+async function add(interaction: CommandInteraction) {
 
+}
+
+async function remove(interaction: CommandInteraction) {
+	
+}
+
+async function list(interaction: CommandInteraction) {
+	
+}
+
+const subcommands: Record<string, (interaction: CommandInteraction) => Promise<void>> = { 
+	'add': add, 
+	'remove': remove, 
+	'list': list
+};
+
+export const execute = async (interaction: CommandInteraction) => {           
+	if(!subcommands[interaction.options.getSubcommand()]) throw new Error(`Invalid subcommand: ${interaction.options.getSubcommand()}`);
+
+	if(!interaction.guildId) throw new Error("Invalid guild id...");
+	if(!interaction.guild) throw new Error('Can\'t have guild in Detroit');
+
+	subcommands[interaction.options.getSubcommand()](interaction);
 }
