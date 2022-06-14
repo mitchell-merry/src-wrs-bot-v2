9e28f65a0ba1b0f6585406d373f74dfd0d7105b6
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-14 18:34:34 +1000

    Get leaderboards tracked by guild and the associated leaderboard/variables.

--- src/discord/commands/update.command.ts
+++ src/discord/commands/update.command.ts
@@ -1,5 +1,8 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
+import { DB } from "../../db";
+import { GuildEntity, TrackedLeaderboard } from "../../db/models";
+import UserError from "../UserError";
 
 export const data = new SlashCommandBuilder()
 	.setName('update')
@@ -7,6 +10,21 @@ export const data = new SlashCommandBuilder()
 
 export const perms = 'mods';
 
-export const execute = async (interaction: CommandInteraction) => {        
+export const execute = async (interaction: CommandInteraction) => {
+	// check if guild is tracked
+	if(!interaction.guild || !interaction.guildId) throw new Error('guild or guildId undefined.');
+	const guildRepo = DB.getRepository(GuildEntity);
+	const guildEnt = await guildRepo.findOne({ where: { guild_id: interaction.guildId }});
+	if(!guildEnt) throw new UserError('Error: this guild is not being tracked!');
+
+	// get all leaderboards tracked by guild
+	const tlbRepo = DB.getRepository(TrackedLeaderboard);
+	const tracked = await tlbRepo.find({
+		where: { guild_id: interaction.guildId },
+		relations: {
+			leaderboard: { variables: true }
+		}
+	});
+
 	interaction.deferReply();
 }
