b47690059e8866cb1776270a730401d243a6e92f
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-14 19:54:18 +1000

    Get record run(s) of every leaderboard.

--- src/discord/commands/update.command.ts
+++ src/discord/commands/update.command.ts
@@ -3,6 +3,7 @@ import { CommandInteraction } from "discord.js";
 import { DB } from "../../db";
 import { GuildEntity, TrackedLeaderboard } from "../../db/models";
 import UserError from "../UserError";
+import * as SRC from '../../speedruncom';
 
 export const data = new SlashCommandBuilder()
 	.setName('update')
@@ -11,6 +12,8 @@ export const data = new SlashCommandBuilder()
 export const perms = 'mods';
 
 export const execute = async (interaction: CommandInteraction) => {
+	interaction.deferReply();
+
 	// check if guild is tracked
 	if(!interaction.guild || !interaction.guildId) throw new Error('guild or guildId undefined.');
 	const guildRepo = DB.getRepository(GuildEntity);
@@ -33,5 +36,24 @@ export const execute = async (interaction: CommandInteraction) => {
 		roleLeaderboards[tlb.role_id].push(tlb);
 	});
 	
-	interaction.deferReply();
+	// for every role (wait for all)
+	await Promise.all(Object.entries(roleLeaderboards).map(async ([ roleId, tlbs ]) => {
+		// fetch role
+		let role = await interaction.guild!.roles.fetch(roleId);
+		
+		// TODO give the user the option of making a new role, attaching an existing role, removing the leaderboard, or ignoring altogether
+		if(!role) throw new UserError(`ERROR: role no exist.`);
+
+		// list of accounts to add the role to
+		const accounts = [];
+		await Promise.all(tlbs.map(async tlb => {
+
+			// format variables
+			const variables = Object.fromEntries(tlb.leaderboard.variables.map(variable => {
+				return [ `var-${variable.variable_id}`, variable.value ]
+			}));
+
+			console.log(await SRC.getLeaderboard(tlb.leaderboard.game_id, tlb.leaderboard.category_id, { top: 1, ...variables }));
+		}));
+	}));
 }
