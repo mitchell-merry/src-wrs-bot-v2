4b09d8ce355dcd36caffe47ef92c088d548fab20
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-18 23:02:41 +1000

    Add /leaderboard setrole. (#8)
    * Add /leaderboard setrole command.
    
    * Change role option to role type, as intended.
    
    * Use remove autocomplete in setrole.
    
    * Add lb.setrole.ts, mistakenly missing from prev commits.
    
    * Implement setrole.

---
+++ src/discord/commands/leaderboard/lb.setrole.ts
@@ -0,0 +1,31 @@
+import { CommandInteraction, Role, User } from "discord.js";
+import { DB } from "../../../db";
+import { Leaderboard, TrackedLeaderboard } from "../../../db/models";
+import UserError from "../../UserError";
+
+export async function setrole(interaction: CommandInteraction) {
+	await interaction.deferReply();
+	
+	const tlbRepo = DB.getRepository(TrackedLeaderboard);
+	const lbRepo = DB.getRepository(Leaderboard);
+	
+	const lb_id = interaction.options.getInteger('leaderboard')!;
+	const new_role = interaction.options.getRole('role') as Role;
+
+	const lbCrit = { where: { lb_id }, relations: { trackedLeaderboards: true } };
+	const lb = await lbRepo.findOne(lbCrit);
+	if(!lb) throw new UserError(`That leaderboard does not exist.`);
+
+	const tlbCriteria = { where: { guild_id: interaction.guildId!, lb_id } }
+	const tlb = await tlbRepo.findOne(tlbCriteria);
+	if(!tlb) throw new UserError(`This guild does not track the given leaderboard.`);
+
+	if(new_role!.position > interaction.guild!.me!.roles.highest.position)
+	{
+		throw new UserError('Bot does not have permission to modify the specified role.');
+	}
+
+	tlb.role_id = new_role.id;
+	await tlbRepo.save(tlb);
+	interaction.editReply('Leaderboard updated. Run /update to reflect this change in role holders.');
+}
--- src/discord/commands/leaderboard/leaderboard.command.ts
+++ src/discord/commands/leaderboard/leaderboard.command.ts
@@ -3,6 +3,7 @@ import { CommandInteraction } from "discord.js";
 import { add } from "./lb.add";
 import { list } from "./lb.list";
 import { remove } from "./lb.remove";
+import { setrole } from "./lb.setrole";
 
 export const data = new SlashCommandBuilder()
 	.setName('leaderboard')
@@ -18,18 +19,25 @@ export const data = new SlashCommandBuilder()
 		.addBooleanOption(o => o.setName('delete_role').setDescription('Delete the associated role.')))
 	.addSubcommand(sc => sc
 		.setName('list')
-		.setDescription('Lists all leaderboards tracked in this guild.'));
+		.setDescription('Lists all leaderboards tracked in this guild.'))
+	.addSubcommand(sc => sc
+		.setName('setrole')
+		.setDescription('Sets the role for the leaderboard.')
+		.addIntegerOption(o => o.setName('leaderboard').setDescription('The leaderboard to change the role of.').setRequired(true).setAutocomplete(true))
+		.addRoleOption(o => o.setName('role').setDescription('The role to change to.').setRequired(true)));
 
 export const perms = {
 	'add': 'mods',
 	'remove': 'mods',
-	'list': 'all'
+	'list': 'all',
+	'setrole': 'mods',
 };
 
 const subcommands: Record<string, (interaction: CommandInteraction) => Promise<void>> = { 
 	'add': add, 
 	'remove': remove, 
-	'list': list
+	'list': list,
+	'setrole': setrole,
 };
 
 export const execute = async (interaction: CommandInteraction) => {           
