9474900bc74915c71ed770c747340b4473f695d7
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-19 00:13:43 +1000

    Add clearCommands script.

---
+++ src/clear_commands.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+
+if(!process.env.TOKEN || !process.env.client) throw new Error('TOKEN and client environment variables are required.');
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+console.log('Started clearing application (/) commands.');
+
+rest.put(
+	process.env.guild 
+		? Routes.applicationGuildCommands(process.env.client, process.env.guild)
+		: Routes.applicationCommands(process.env.client),
+	{ body: [] },
+).then(() => console.log('Successfully cleared application (/) commands.'))
+.catch(error => console.error(error));
