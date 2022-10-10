c1c23d8279e8ebb91126677046ca64eb8d1cfb6c
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-10 18:06:33 +1100

    Add more logging

--- src/discord/commands/update.command.ts
+++ src/discord/commands/update.command.ts
@@ -45,11 +45,15 @@ export const execute = async (interaction: CommandInteraction) => {
 		
 		// TODO give the user the option of making a new role, attaching an existing role, removing the leaderboard, or ignoring altogether
 		if(!role) throw new UserError(`ERROR: role no exist.`);
-		console.log(`[${roleId}] Updating @${role.name}`);
+
+		const log = (s: string) => console.log(`[/update] $[${roleId}] ${s}`);
+		log(`Updating @${role.name}`);
 
 		// list of accounts to add the role to
 		let accounts: string[] = [];
 		await Promise.all(tlbs.map(async tlb => {
+			const lblog = (s: string) => log(`:: [${tlb.leaderboard.lb_name}] ${s}`);
+
 			// get all sr.c player ids
 			const partial: SRC.LeaderboardPartial = {
 				game: tlb.leaderboard.game_id,
@@ -62,28 +66,38 @@ export const execute = async (interaction: CommandInteraction) => {
 
 			// guests are ignored
 			const srcPlayerIds = lb.runs.map(run => run.run.players.filter(SRC.playerIsUser).map(p => p.id)).flat();
-			
+			lblog(`Found WR holder(s) (sr.c): ${srcPlayerIds.join(', ')}`);
+
 			const discPlayerIDs = (await Promise.all(srcPlayerIds.map(async id => 
 				await pRepo.findOne({ where: { player_id: id }})
 			))).filter((p): p is PlayerEntity => p !== null).map(p => p.discord_id);
+			lblog(`Found WR holder(s) (discord): ${discPlayerIDs.join(', ')}`);
 			
 			discPlayerIDs.forEach(id => {
 				if(!accounts.includes(id)) accounts.push(id);
 			});
 		}));
 
+		log(`Account(s) should be (discord): ${accounts.join(', ')}`);
+
 		// remove role from accounts that shouldn't have the role, and remove those that already have the role from the accounts list
 		await Promise.all(role.members.map(async member => {
 			if(accounts.includes(member.id)) accounts = accounts.filter(a => a !== member.id);
-			else await member.roles.remove(role!);
+			else
+			{
+				log(`Removing role from ${member.user.username}#${member.user.tag}...`);
+				await member.roles.remove(role!);
+			}
 		}));
 
+		// add roles to users
 		await Promise.all(accounts.map(async a => {
 			const user = await interaction.guild!.members.fetch(a);
+			log(`Adding role to ${user.user.username}#${user.user.tag}...`);
 			await user.roles.add(role!);
 		}));
 		
-		console.log(`[${roleId}] Finished @${role.name}`);
+		log(`Finished @${role.name}!`);
 	}));
 
 	interaction.editReply('Done updating.');
