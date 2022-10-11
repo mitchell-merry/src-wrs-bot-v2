493214fec2c80e6a4be7d7357d8059f3b7f8e92e
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-11 15:25:55 +1100

    Extract paginatedlist functionality from /lb list.

--- src/discord/commands/leaderboard/lb.list.ts
+++ src/discord/commands/leaderboard/lb.list.ts
@@ -1,64 +1,25 @@
 import { Collection, CommandInteraction, Message, MessageActionRow, MessageButton, MessageComponentInteraction, Role, WebhookEditMessageOptions } from "discord.js";
 import { DB } from "../../../db";
 import { TrackedLeaderboardEntity } from "../../../db/entities";
+import PaginatedList from "../../menus/PaginatedList";
 import UserError from "../../UserError";
-import { array_chunks } from "../../util";
-
-const PAGE_LENGTH = 15;
 
 export async function list(interaction: CommandInteraction) {
-	const tlbRepo = DB.getRepository(TrackedLeaderboardEntity);
 	await interaction.deferReply();
-	const message = await interaction.fetchReply() as Message;
+	
+	const tlbRepo = DB.getRepository(TrackedLeaderboardEntity);
 	const roles = await interaction.guild!.roles.fetch();
 
 	// get leaderboards tracked by guild
 	const boards = await tlbRepo.find({ where: { guild_id: interaction.guildId! }, relations: { leaderboard: true } });
 	if(!boards || boards.length === 0) throw new UserError("This guild tracks no leaderboards.");
-	// group boards in chunks
-	const pages = array_chunks(boards, PAGE_LENGTH);
-	let page = 0;
-	let r: MessageComponentInteraction | undefined = undefined;
 
-	while(true) {
-		const options: WebhookEditMessageOptions = {
-			content: pageOutput(pages, page, roles),
-			components: pageComponents(page, pages.length)
-		};
+	// spawn a paginated list
+	const items = boards.map(tlb => {
+		const role = roles.get(tlb.role_id);
+		return `${tlb.leaderboard.lb_name} - <@&${role?.id}>`;
+	});
 
-		await (r 
-			? r.update(options)
-			: interaction.editReply(options)
-		);
-		
-		// listen for button
-		r = await message.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: 300000 })
-			.catch(r => {throw new UserError("This list has expired. Spawn a new one with /leaderboard list.")});
-
-		switch(r.customId) {
-			case(`left_all`): page = 0; break;
-			case(`left_once`): page--; break;
-			case(`right_once`): page++; break;
-			case(`right_all`): page = pages.length-1; break;
-		}
-	}
-}
-
-function pageOutput(pages: TrackedLeaderboardEntity[][], page: number, roles: Collection<string, Role>): string {
-	return `${
-		pages[page].map((tlb, i) => {
-			const role = roles.get(tlb.role_id);
-			return `[${page*PAGE_LENGTH+i+1}] ${tlb.leaderboard.lb_name} - <@&${role?.id}>`;
-		}).join('\n')
-	}`;
-}
-
-function pageComponents(page: number, pageCount: number): MessageActionRow[] {
-	return [new MessageActionRow().addComponents([
-		new MessageButton().setLabel('|<').setCustomId(`left_all`).setDisabled(page === 0).setStyle('PRIMARY'),
-		new MessageButton().setLabel('<').setCustomId(`left_once`).setDisabled(page === 0).setStyle('PRIMARY'),
-		new MessageButton().setLabel(`${page+1} / ${pageCount}`).setCustomId('page').setDisabled(true).setStyle('SECONDARY'),
-		new MessageButton().setLabel('>').setCustomId(`right_once`).setDisabled(page === pageCount-1).setStyle('PRIMARY'),
-		new MessageButton().setLabel('>|').setCustomId(`right_all`).setDisabled(page === pageCount-1).setStyle('PRIMARY'),
-	])];
+	new PaginatedList(items, 15, "This list has expired. Use /leaderboard list to spawn a new one.")
+		.spawnMenu(interaction);
 }
---
+++ src/discord/menus/PaginatedList.ts
@@ -0,0 +1,73 @@
+import { CommandInteraction, Message, MessageActionRow, MessageButton, MessageComponentInteraction, WebhookEditMessageOptions } from "discord.js";
+import UserError from "../UserError";
+
+const MESSAGE_LIMIT = 2000;
+
+export default class PaginatedList {
+	private items: string[];
+	private pages: string[][];
+	private pageSize: number;
+	private currentPage: number;
+	private expireMessage: string;
+
+	constructor(items: string[], pageSize: number = 25, expireMessage = "This list has expired.")
+	{
+		this.items = items.map((item, i) => `[${i+1}] ${item}\n`);
+		this.pages = [[]];
+		this.pageSize = pageSize;
+
+		this.items.forEach(item => {
+			const lastPage = this.pages.at(-1)!;
+			const pageLength = lastPage.reduce((prev, curr) => prev + curr.length, 0);
+			console.log(lastPage.length);
+			
+			if (lastPage.length >= this.pageSize || pageLength + item.length > MESSAGE_LIMIT) this.pages.push([item]);
+			else lastPage.push(item);
+		});
+
+		this.currentPage = 0;
+		this.expireMessage = expireMessage;
+	}
+
+	public async spawnMenu(interaction: CommandInteraction)
+	{
+		const message = await interaction.fetchReply() as Message;
+		let r: MessageComponentInteraction | undefined = undefined;
+
+		while(true) {
+			const options: WebhookEditMessageOptions = {
+				content: this.pages[this.currentPage].join(''),
+				components: this.getPageComponents(this.currentPage)
+			};
+
+			await (r 
+				? r.update(options)
+				: interaction.editReply(options)
+			);
+			
+			// listen for button
+			r = await message.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: 300000 })
+				.catch(r => {throw new UserError(this.expireMessage)});
+
+			switch(r.customId) {
+				case(`left_all`): this.currentPage = 0; break;
+				case(`left_once`): this.currentPage--; break;
+				case(`right_once`): this.currentPage++; break;
+				case(`right_all`): this.currentPage = this.pages.length-1; break;
+			}
+		}
+	}
+
+	private getPageComponents(page: number) {
+		const isFirstPage = page === 0;
+		const isLastPage = page === this.pages.length - 1;
+
+		return [new MessageActionRow().addComponents([
+			new MessageButton().setLabel('|<').setCustomId(`left_all`).setDisabled(isFirstPage).setStyle('PRIMARY'),
+			new MessageButton().setLabel('<').setCustomId(`left_once`).setDisabled(isFirstPage).setStyle('PRIMARY'),
+			new MessageButton().setLabel(`${page+1} / ${this.pages.length}`).setCustomId('page').setDisabled(true).setStyle('SECONDARY'),
+			new MessageButton().setLabel('>').setCustomId(`right_once`).setDisabled(isLastPage).setStyle('PRIMARY'),
+			new MessageButton().setLabel('>|').setCustomId(`right_all`).setDisabled(isLastPage).setStyle('PRIMARY'),
+		])];
+	}
+}
