c13f7e8d4d804cc9f46e582bd09d7da86106313b
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-18 09:43:21 +1100

    Add initial LeaderboardMenu code

---
+++ src/discord/menus/LeaderboardMenu.ts
@@ -0,0 +1,36 @@
+import { CommandInteraction } from "discord.js";
+import * as SRC from "src-ts";
+import UserError from "../UserError";
+import DialogueMenu from "./DialogueMenu";
+
+export default class LeaderboardMenu {
+	private static readonly types = [{
+		id: 'per-game',
+		label: "Full-game"
+	}, { 
+		id: 'per-level',
+		label: "Level"
+	}];
+
+	constructor() {
+
+	}
+
+	public async spawnMenu(interaction: CommandInteraction, gameId: string) {
+		const game = await SRC.getGame<'categories.variables,levels'>(gameId, { embed: 'categories.variables,levels' });
+
+		let type: SRC.CategoryType;
+
+		if (game.levels.data.length === 0 && game.categories.data.length === 0)
+			throw new UserError(`The game ${game.names.international} has no leaderboards!`);
+
+		if (game.levels.data.length !== 0 && game.categories.data.length !== 0) {
+			type = (await new DialogueMenu(`Is the leaderboard a full-game or level category?`, LeaderboardMenu.types, "PRIMARY")
+				.spawnMenu(interaction, "NEW_DELETE")) as SRC.CategoryType;
+		}
+		else if (game.levels.data.length === 0) type = "per-game";
+		else type = "per-level";
+
+		
+	}
+}
