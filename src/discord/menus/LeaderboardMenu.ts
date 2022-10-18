79f0ad89ead53222b35ed9268e35ae21f3b6fd15
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-18 22:48:12 +1100

    Output variables on selection, also show global variables as an option

--- src/discord/menus/LeaderboardMenu.ts
+++ src/discord/menus/LeaderboardMenu.ts
@@ -74,15 +74,22 @@ export default class LeaderboardMenu {
 	}
 
 	public async selectVariables(interaction: CommandInteraction, message: string, variables: SRC.Variable[], level?: SRC.Level): Promise<[SRC.Variable, string][]> {
+		const selectedVariables: string[] = [];
+
 		return Promise.all(variables.filter(SRC.variableIsSubcategory)
 			.filter(v => level === undefined 
+				|| v.scope.type === 'global'
 				|| v.scope.type === 'all-levels'
 				|| (v.scope.type === 'single-level' && v.scope.level === level.id)
 			).map(async subcat => {
 				const options = Object.entries(subcat.values.values)
 					.map(([k, v]) => ({ id: k, label: v.label }));
 	
-				const [ value ] = await new DialogueMenu(`Choose a value for the variable ${subcat.name}:`, options).spawnMenu(interaction, "NEW_REPLY");
+				const [ value, valueLabel ] = await new DialogueMenu(`Choose a value for the variable ${subcat.name}:`, options).spawnMenu(interaction, "NEW_REPLY");
+				
+				selectedVariables.push(`Selected the value ${valueLabel} [${value}] for variable ${subcat.name} [${subcat.id}].`);
+				interaction.editReply(`${message}\n${selectedVariables.join('\n')}`);
+				
 				return [ subcat, value ];
 			})
 		);
