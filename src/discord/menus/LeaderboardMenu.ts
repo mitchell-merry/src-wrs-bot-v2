a3a08a213f82f2462923f2264b437cc029c95570
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-18 15:05:22 +1100

    Prototype working version

--- src/discord/menus/LeaderboardMenu.ts
+++ src/discord/menus/LeaderboardMenu.ts
@@ -1,4 +1,5 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, MessageComponentInteraction } from "discord.js";
+import { type } from "os";
 import * as SRC from "src-ts";
 import UserError from "../UserError";
 import DialogueMenu from "./DialogueMenu";
@@ -11,52 +12,54 @@ export default class LeaderboardMenu {
 
 	constructor() { }
 
-	public async spawnMenu(interaction: CommandInteraction, gameId: string) {
+	public async spawnMenu(interaction: CommandInteraction, gameId: string): Promise<[{ game: SRC.Game, level: SRC.Level | undefined, category: SRC.Category, variables: [SRC.Variable, string][] }, MessageComponentInteraction | undefined]> {
 		const game = await SRC.getGame<'categories.variables,levels'>(gameId, { embed: 'categories.variables,levels' });
-		const [ type, label ] = await this.selectType(interaction, game);
-		let message = label ? '' : `Selected "${label}".\n`;
+		let [ type, mciT ] = await this.selectType(interaction, game);
+		let message = `Selected "${LeaderboardMenu.types[type]}".\n`;
 
-		const level = type === "per-level" ? await this.selectLevel(interaction, game.levels.data, message) : undefined;
-		if (level) message += `Selected the level "${level.name}"\n`;
+		const [ level, mciL ] = type === "per-level" ? await this.selectLevel(interaction, game.levels.data, message, mciT) : [];
+		if (level) message += `Selected the level "${level.name}".\n`;
 
-		const category = await this.selectCategory(interaction, game.categories.data, type, message);
+		const [ category, mciC ] = await this.selectCategory(interaction, game.categories.data, type, message, mciL ?? mciT);
+		message += `Selected the category "${category.name}"\n`;
+		(mciC ?? (mciL ?? mciT))?.update({ content: message }) ?? interaction.editReply({ content: message });
+
 		const variables = await this.selectVariables(interaction, category.variables.data, level);
 
-		return {
-			game, level, category, variables
-		};
+		return [ { game, level, category, variables }, mciC ?? (mciL ?? mciT) ];
 	}
 
-	public async selectType(interaction: CommandInteraction, game: SRC.Game<'categories.variables,levels'>): Promise<[SRC.CategoryType, string] | [SRC.CategoryType]> {
+	public async selectType(interaction: CommandInteraction, game: SRC.Game<'categories.variables,levels'>): Promise<[SRC.CategoryType, MessageComponentInteraction] | [SRC.CategoryType]> {
 		if (game.levels.data.length === 0 && game.categories.data.length === 0)
 			throw new UserError(`The game ${game.names.international} has no leaderboards!`);
 
 		if (game.levels.data.length !== 0 && game.categories.data.length !== 0) {
-			return await new DialogueMenu<SRC.CategoryType>(`Is the leaderboard a full-game or level category?`, LeaderboardMenu.types, "PRIMARY")
+			const [type, _, mci] = await new DialogueMenu<SRC.CategoryType>(`Is the leaderboard a full-game or level category?`, LeaderboardMenu.types, "PRIMARY")
 				.spawnMenu(interaction, "EDIT_REPLY");
+			return [type, mci];
 		}
 		else if (game.levels.data.length === 0) return [ "per-game" ];
 		else return [ "per-level" ];
 	}
 
-	public async selectLevel(interaction: CommandInteraction, levels: SRC.Level[], message: string) {
-		if (levels.length === 1) return levels[0];
+	public async selectLevel(interaction: CommandInteraction, levels: SRC.Level[], message: string, mci?: MessageComponentInteraction): Promise<[SRC.Level, MessageComponentInteraction] | [SRC.Level]> {
+		if (levels.length === 1) return [ levels[0] ];
 		
-		let q = (message === '' ? `\n${message}` : '') + 'Choose a level:';
+		let q = (message !== '' ? `${message}\n` : '') + 'Choose a level:';
 		const levelOptions = levels.map(level => ({ id: level.id, label: level.name }));
-		const [ levelId ] = await new DialogueMenu(q, levelOptions, "PRIMARY").spawnMenu(interaction, "EDIT_REPLY");
-
-		return levels.find(l => l.id === levelId)!;
+		const [ levelId, _, r ] = await new DialogueMenu(q, levelOptions, "PRIMARY").spawnMenu(interaction, "EDIT_REPLY", mci);
+		
+		return [ levels.find(l => l.id === levelId)!, r ];
 	}
 
-	public async selectCategory<E extends string, T extends SRC.CategoryType>(interaction: CommandInteraction, categories: SRC.Category<E, T>[], type: T, message: string) {
-		if (categories.length === 1) return categories[0];
+	public async selectCategory<E extends string, T extends SRC.CategoryType>(interaction: CommandInteraction, categories: SRC.Category<E, T>[], type: T, message: string, mci?: MessageComponentInteraction): Promise<[SRC.Category<E>, MessageComponentInteraction] | [SRC.Category<E>]> {
+		if (categories.length === 1) return [ categories[0] ];
 
-		let q = (message === '' ? `\n${message}` : '') + 'Choose a category:';
+		let q = (message !== '' ? `${message}\n` : '') + 'Choose a category:';
 		const catOptions = categories.filter(c => c.type === type).map(c => ({ id: c.id, label: c.name }));
-		const [ categoryId ] = await new DialogueMenu(q, catOptions, "PRIMARY").spawnMenu(interaction, "EDIT_REPLY");
+		const [ categoryId, _, r ] = await new DialogueMenu(q, catOptions, "PRIMARY").spawnMenu(interaction, "EDIT_REPLY", mci);
 
-		return categories.find(c => c.id === categoryId)!;
+		return [ categories.find(c => c.id === categoryId)!, r ];
 	}
 
 	public async selectVariables(interaction: CommandInteraction, variables: SRC.Variable[], level?: SRC.Level): Promise<[SRC.Variable, string][]> {
@@ -68,7 +71,7 @@ export default class LeaderboardMenu {
 				const options = Object.entries(subcat.values.values)
 					.map(([k, v]) => ({ id: k, label: v.label }));
 	
-				const [ value ] = await new DialogueMenu(`Choose a value for the variable ${subcat.name}:`, options, "PRIMARY").spawnMenu(interaction, "NEW_MESSAGE");
+				const [ value ] = await new DialogueMenu(`Choose a value for the variable ${subcat.name}:`, options, "PRIMARY").spawnMenu(interaction, "NEW_REPLY");
 				return [ subcat, value ];
 			})
 		);
