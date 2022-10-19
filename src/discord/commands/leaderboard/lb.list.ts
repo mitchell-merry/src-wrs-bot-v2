790c421557b43f9053666198c1f6352acd29f1b1
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-19 15:39:28 +1100

    Update /leaderboard to new API including autocompleter.

--- src/discord/commands/leaderboard/lb.list.ts
+++ src/discord/commands/leaderboard/lb.list.ts
@@ -1,25 +1,34 @@
+import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
 import {  CommandInteraction } from "discord.js";
 import { DB } from "../../../db";
 import { TrackedLeaderboardEntity } from "../../../db/entities";
 import PaginatedList from "../../menus/PaginatedList";
 import UserError from "../../UserError";
+import { Subcommand } from "../Command";
 
-export async function list(interaction: CommandInteraction) {
-	await interaction.deferReply();
-	
-	const tlbRepo = DB.getRepository(TrackedLeaderboardEntity);
-	const roles = await interaction.guild!.roles.fetch();
+const LeaderboardListCommand: Subcommand = {
+	data: new SlashCommandSubcommandBuilder().setName('list')
+		.setDescription('Lists all leaderboards tracked in this guild.'),
+	perm: 'all',
+	execute: async (interaction: CommandInteraction) => {
+		await interaction.deferReply();
+		
+		const tlbRepo = DB.getRepository(TrackedLeaderboardEntity);
+		const roles = await interaction.guild!.roles.fetch();
 
-	// get leaderboards tracked by guild
-	const boards = await tlbRepo.find({ where: { guild_id: interaction.guildId! }, relations: { leaderboard: true } });
-	if(!boards || boards.length === 0) throw new UserError("This guild tracks no leaderboards.");
+		// get leaderboards tracked by guild
+		const boards = await tlbRepo.find({ where: { guild_id: interaction.guildId! }, relations: { leaderboard: true } });
+		if(!boards || boards.length === 0) throw new UserError("This guild tracks no leaderboards.");
 
-	// spawn a paginated list
-	const items = boards.map(tlb => {
-		const role = roles.get(tlb.role_id);
-		return `${tlb.leaderboard.lb_name} - <@&${role?.id}>`;
-	});
+		// spawn a paginated list
+		const items = boards.map(tlb => {
+			const role = roles.get(tlb.role_id);
+			return `${tlb.leaderboard.lb_name} - <@&${role?.id}>`;
+		});
 
-	await new PaginatedList(items, 15, "This list has expired. Use /leaderboard list to spawn a new one.")
-		.spawnMenu(interaction);
-}
+		await new PaginatedList(items, 15, "This list has expired. Use /leaderboard list to spawn a new one.")
+			.spawnMenu(interaction);
+	}
+};
+
+export default LeaderboardListCommand;
