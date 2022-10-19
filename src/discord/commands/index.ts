9d2e72d54d5004f94625b700c19b1d99de41c1f7
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-19 16:12:16 +1100

    Convert rest of code

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,50 +1,82 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { AutocompleteInteraction, CommandInteraction, WebhookEditMessageOptions } from 'discord.js';
-
-export type PermissionLevel = 'admin' | 'mods' | 'all';
-
-export interface CommandFile {
-	data: SlashCommandBuilder;
-	perms: Record<string, PermissionLevel> | PermissionLevel;
-	execute: (interaction: CommandInteraction) => Promise<void>;
-}
-
-import * as invite from './invite.command';
-import * as modroles from './modroles.command';
-import * as set from './set.command';
-import * as player from './player.command';
-import * as leaderboard from './leaderboard/leaderboard.command';
-import * as update from './update.command';
-import UserError from '../UserError';
-import { DB, isUserMod } from '../../db';
-import { TrackedLeaderboardEntity } from '../../db/entities';
+import { AutocompleteInteraction, CommandInteraction, Interaction, WebhookEditMessageOptions } from 'discord.js';
 import SRCError from 'src-ts/lib/src/SRCError';
 
-export const commands = [ invite, modroles, set, player, leaderboard, update ] as CommandFile[];
+import InviteCommand from './invite.command';
+import ModrolesCommand from './modroles.command';
+import SetCommand from './set.command';
+import PlayerCommand from './player.command';
+import LeaderboardCommand from './leaderboard/leaderboard.command';
+import UpdateCommand from './update.command';
+import UserError from '../UserError';
+import { DB, isUserMod } from '../../db';
+import { Command, CommandWithSubcommands, Executer, PermissionLevel } from './Command';
+import { GuildEntity } from '../../db/entities';
+
+export const commands = [ InviteCommand, ModrolesCommand, SetCommand, PlayerCommand, LeaderboardCommand, UpdateCommand ];
+
+export const hasSubcommands = (cmd: Command | CommandWithSubcommands): cmd is CommandWithSubcommands => 'subcommands' in cmd;
 
 export async function handleSlashCommand(interaction: CommandInteraction) {
-	const command = commands.find((c) => c.data.name === interaction.commandName);
+	const command = commands.find(c => c.data.name === interaction.commandName);
+	if (!command) throw new Error(`Command ${interaction.commandName} unknown`);
+	
+	let perm: PermissionLevel;
+	let execute: Executer;
 
-	try {
-
-		if(!command) throw new Error(`Command ${interaction.commandName} unknown`);
-
-		// Get the perm level of the command or subcommand.
-		let permLevel;
-		if(typeof command.perms === 'string') permLevel = command.perms;
-		else permLevel = command.perms[interaction.options.getSubcommand()];
+	if (hasSubcommands(command)) {
+		const sc = command.subcommands.find(sc => sc.data.name === interaction.options.getSubcommand());
+		if(!sc) throw new Error(`Invalid subcommand: ${interaction.options.getSubcommand()}`);
 		
-		if(!permLevel) throw new Error(`Permission level missing for ${interaction.options.getSubcommand()}.`);
-		if(typeof interaction.member?.permissions === 'string') throw new Error(`error with ${interaction.member?.permissions}`)
+		perm = sc.perm;
+		execute = sc.execute;
+	} else {
+		perm = command.perm;
+		execute = command.execute;
+	}
+	
+	if (typeof interaction.member?.permissions === 'string') throw new Error(`error with ${interaction.member?.permissions}`)
 
-		const userIsAdmin = interaction.user.id === process.env.admin || interaction.member!.permissions.has('ADMINISTRATOR');
-		const userIsMod = userIsAdmin || (await isUserMod(interaction.guildId, interaction.member));
+	const userIsAdmin = interaction.user.id === process.env.admin || interaction.member!.permissions.has('ADMINISTRATOR');
+	const userIsMod = userIsAdmin || (await isUserMod(interaction.guildId, interaction.member));
 
-		// Check user has correct permission.
-		if(permLevel === 'admin' && !userIsAdmin) throw new UserError(`Only admins are allowed to use this command! Loser. Scram!!`);
-		else if(permLevel === 'mods' && !userIsMod) throw new UserError(`Only mods and above are allowed to use this! Shame on you. Bad.`);
+	// Check user has correct permission.
+	if (perm === 'admin' && !userIsAdmin)
+		throw new UserError(`Only admins are allowed to use this command! Loser. Scram!!`);
+	else if (perm === 'mods' && !userIsMod)
+		throw new UserError(`Only mods and above are allowed to use this! Shame on you. Bad.`);
 
-		await command.execute(interaction);
+	if (!interaction.guild || !interaction.guildId)
+		throw new Error('For some reason missing guild');
+
+	const gRepo = DB.getRepository(GuildEntity);
+	const guildEnt = await gRepo.findOne({ where: { guild_id: interaction.guildId }});
+	if (!guildEnt)
+		throw new Error('Guild not being tracked as an entity for some reason.');
+
+	await execute(interaction, guildEnt);
+}
+
+export async function handleAutocomplete(interaction: AutocompleteInteraction) {
+	const command = commands.find(c => c.data.name === interaction.commandName);
+	if (!command) throw new Error(`Command ${interaction.commandName} unknown`);
+	
+	let acl;
+	if (hasSubcommands(command)) {
+		const sc = command.subcommands.find(sc => sc.data.name === interaction.options.getSubcommand());
+		if(!sc) throw new Error(`Invalid subcommand: ${interaction.options.getSubcommand()}`);
+		
+		acl = sc.autocomplete;
+	} else {
+		acl = command.autocomplete;
+	}
+
+	if (!acl) throw new Error(`Error getting autocomplete: Missing autocompleter`);
+}
+
+export async function interactionCreate(interaction: Interaction) {
+	try {
+		if(interaction.isCommand()) await handleSlashCommand(interaction);
+		if(interaction.isAutocomplete()) await handleAutocomplete(interaction); 
 	} catch (error) {
 		const data: WebhookEditMessageOptions = {
 			content: "Unknown error occurred.",
@@ -58,17 +90,10 @@ export async function handleSlashCommand(interaction: CommandInteraction) {
 			console.error(error);
 		}
 
-		await ((interaction.replied || interaction.deferred)
-			? interaction.editReply(data) 
-			: interaction.reply(data));
-	}
-}
-
-export async function handleAutocomplete(interaction: AutocompleteInteraction) {
-	// TODO: Make more modular
-	if(interaction.commandName === 'leaderboard'
-		&& (interaction.options.getSubcommand() === 'remove' || interaction.options.getSubcommand() === 'setrole')
-	) {
-		
+		if (interaction.isCommand()) {
+			await ((interaction.replied || interaction.deferred)
+				? interaction.editReply(data) 
+				: interaction.reply(data));
+		}
 	}
 }
