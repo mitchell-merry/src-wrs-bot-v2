dd34a77d3f6933cc83b2b231ec1e9dbc57c893c0
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-19 20:32:08 +1100

    Fix /set list bug

--- src/discord/commands/set/set.list.ts
+++ src/discord/commands/set/set.list.ts
@@ -11,7 +11,7 @@ const SetListCommand: Subcommand = {
 		msg += `\`above_role\`: `;
 		const roleDiscordObj = interaction.guild!.roles.cache.get(guildEnt.above_role_id);
 		if(!roleDiscordObj || !guildEnt.above_role_id) msg += 'Not set! Will stick to bottom.\n';
-		else msg += `${roleDiscordObj.name} [${guildEnt.above_role_id}]\n`;
+		else msg += `<@&${guildEnt.above_role_id}>\n`;
 	
 		msg += `\`role_default_colour\`: ${guildEnt.role_default_colour}`;
 	
