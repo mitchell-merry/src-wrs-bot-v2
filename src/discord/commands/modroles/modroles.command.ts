482d1ec0ae7663e9740d31b0a12c3a4a871ee031
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-19 20:06:35 +1100

    Add /modroles prune.

--- src/discord/commands/modroles/modroles.command.ts
+++ src/discord/commands/modroles/modroles.command.ts
@@ -2,13 +2,14 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandWithSubcommands } from "../command";
 import ModrolesAddCommand from "./mr.add";
 import ModrolesListCommand from "./mr.list";
+import ModrolesPruneCommand from "./mr.prune";
 import ModrolesRemoveCommand from "./mr.remove";
 
 const ModrolesCommand: CommandWithSubcommands = {
 	data: new SlashCommandBuilder()
 		.setName('modroles')
 		.setDescription('Manages the moderator roles for the bot. Requires admin permissions.'),
-	subcommands: [ ModrolesAddCommand, ModrolesRemoveCommand, ModrolesListCommand ]
+	subcommands: [ ModrolesAddCommand, ModrolesRemoveCommand, ModrolesListCommand, ModrolesPruneCommand ]
 };
 
 export default ModrolesCommand;
---
+++ src/discord/commands/modroles/mr.prune.ts
@@ -0,0 +1,25 @@
+import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
+import { DB } from "../../../db";
+import { GuildEntity, ModeratorRoleEntity } from "../../../db/entities";
+import UserError from "../../UserError";
+import { Subcommand } from "../command";
+
+const ModrolesPruneCommand: Subcommand = {
+	data: new SlashCommandSubcommandBuilder().setName('prune')
+		.setDescription('Removes all deleted (stale) roles from the modrole list.'),
+	perm: 'admin',
+	execute: async (interaction, guildEnt) => {
+		await interaction.deferReply();
+		await interaction.guild!.roles.fetch();
+
+		const deletedRoles = guildEnt.moderatorRoles.filter(role => !interaction.guild!.roles.cache.get(role.role_id));
+
+		if (deletedRoles.length === 0)
+			throw new UserError('This guild has no stale moderator roles.');
+		
+		await DB.getRepository(ModeratorRoleEntity).remove(deletedRoles);
+		await interaction.editReply(`Pruned ${deletedRoles.length} stale roles. Thank you for shopping at Woolies.`);
+	}
+}
+
+export default ModrolesPruneCommand;
