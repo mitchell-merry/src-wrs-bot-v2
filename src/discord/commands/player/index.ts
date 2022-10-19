62ed5a07bd0cac5c680fbd205bc102455fba82c4
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-19 17:12:43 +1100

    Move player main command into folder and refactor

---
+++ src/discord/commands/player/index.ts
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+import { CommandWithSubcommands } from "../command";
+import PlayerAddCommand from "./p.add";
+import PlayerListCommand from "./p.list";
+import PlayerRemoveCommand from "./p.remove";
+
+const PlayerCommand: CommandWithSubcommands = {
+	data: new SlashCommandBuilder().setName('player')
+		.setDescription('Manages player associations between discord and speedrun.com.'),
+	subcommands: [ PlayerAddCommand, PlayerRemoveCommand, PlayerListCommand ]
+};
+
+export default PlayerCommand;
---
+++ src/discord/commands/player/p.remove.ts
@@ -0,0 +1,26 @@
+import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
+import { DB } from "../../../db";
+import { PlayerEntity } from "../../../db/entities";
+import UserError from "../../UserError";
+import { Subcommand } from "../command";
+
+const PlayerRemoveCommand: Subcommand = {
+	data: new SlashCommandSubcommandBuilder().setName('remove')
+		.setDescription('Remove a player association.')
+		.addUserOption(o => o.setName('user').setDescription('The discord account.').setRequired(true)),
+	perm: 'mods',
+	execute: async (interaction, guildEnt) => {
+		const userOpt = interaction.options.getUser('user');
+		if(!userOpt)
+			throw new UserError('The option user must be set.');
+	
+		let exists = guildEnt.players.find(p => p.discord_id === userOpt.id);
+		if(!exists)
+			throw new UserError(`This discord account is not associated with a speedrun.com account.`);
+	
+		await DB.getRepository(PlayerEntity).remove(exists);
+		await interaction.reply({ content: `Association for <@${userOpt.id}> removed!`, allowedMentions: { users: [], roles: [] } });
+	}
+}
+
+export default PlayerRemoveCommand;
