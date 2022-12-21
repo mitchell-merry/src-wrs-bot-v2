b1e7f09a3a52725099ad198497a3aa13a486a635
Mitchell Merry <mitchellmerry145@gmail.com>
2022-12-22 09:10:28 +1100

    Better typescript

--- src/discord/commands/set/set.rdc.ts
+++ src/discord/commands/set/set.rdc.ts
@@ -5,6 +5,8 @@ import { GuildEntity } from "../../../db/entities";
 import UserError from "../../UserError";
 import { Subcommand } from "../command";
 
+const isHexColorString = (str: string): str is HexColorString => !!str.match(/^#[0-9A-Fa-f]{6}$/);
+
 const SetRoleDefaultColourCommand: Subcommand = {
 	data: new SlashCommandSubcommandBuilder().setName('role_default_colour')
 		.setDescription('The default colour of new roles created by the bot. Should be a hexcode, e.g. #FEE75C.')
@@ -13,9 +15,9 @@ const SetRoleDefaultColourCommand: Subcommand = {
 	execute: async (interaction, guildEnt) => {
 		const colour = interaction.options.getString('role_default_colour');
 		if(!colour) throw new Error('/set role_default_colour: role_default_colour not set / is undefined.');
-		if(!colour.match(/^#[0-9A-Fa-f]{6}$/)) throw new UserError('Colour must be a hexcode, e.g. #FEE75C.');
+		if(!isHexColorString(colour)) throw new UserError('Colour must be a hexcode, e.g. #FEE75C.');
 	
-		guildEnt.role_default_colour = colour as HexColorString;
+		guildEnt.role_default_colour = colour;
 		await DB.getRepository(GuildEntity).save(guildEnt);
 		await interaction.reply(`role_default_colour set to ${colour}.`);
 	}
