1b0c2b771f97352a6cefed99a06e3cb4405c9a6b
Mitchell Merry <mitchellmerry145@gmail.com>
2023-04-17 10:18:41 +1000

    Basic updates

--- src/discord/commands/update.command.ts
+++ src/discord/commands/update.command.ts
@@ -49,7 +49,7 @@ const UpdateCommand: Command = {
 					variables: Object.fromEntries(tlb.leaderboard.variables.map(variable => [ variable.variable_id, variable.value ]))
 				};
 
-				const lb = await SRC.getLeaderboardFromPartial(partial, { top: 1 }).catch(() => { throw new Error(`Error updating ${tlb.leaderboard.lb_name}`) });
+				const lb = await SRC.getLeaderboardFromPartial(partial, { top: 1 }, { cache: false }).catch(() => { throw new Error(`Error updating ${tlb.leaderboard.lb_name}`) });
 
 				// guests are ignored
 				const srcPlayerIds = lb.runs.map(run => run.run.players.filter(SRC.playerIsUser).map(p => p.id)).flat();
