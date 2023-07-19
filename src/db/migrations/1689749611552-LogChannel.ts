2654500bb87014cfcdae993a2186ee16ef735a2d
Mitchell Merry <mitchellmerry145@gmail.com>
2023-07-19 18:19:52 +1000

    AHHH!!!!

--- src/db/migrations/1689749611552-LogChannel.ts
+++ src/db/migrations/1689749611552-LogChannel.ts
@@ -4,8 +4,8 @@ export class LogChannel1689749611552 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         try {
-            await queryRunner.query(`ALTER TABLE \`guild\` ADD \`log_channel\` varchar(255) NOT NULL`);
-            await queryRunner.query('UPDATE \`guild\` SET \`log_channel\` = ');
+            await queryRunner.query(`ALTER TABLE \`guild\` ADD \`log_channel_id\` varchar(255) NOT NULL`);
+            await queryRunner.query('UPDATE \`guild\` SET \`log_channel_id\` = ');
         } catch (e) {
             // TODO LOL!
             if (!(e instanceof QueryFailedError)) throw e;
@@ -13,7 +13,7 @@ export class LogChannel1689749611552 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`guild\` DROP COLUMN \`log_channel\``);
+        await queryRunner.query(`ALTER TABLE \`guild\` DROP COLUMN \`log_channel_id\``);
     }
 
 }
