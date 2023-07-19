7e9a374a2b8d84a17b5dc1f19448afdab83830f4
Mitchell Merry <mitchellmerry145@gmail.com>
2023-07-19 17:44:22 +1000

    Missing from previous

--- src/discord/commands/set/index.ts
+++ src/discord/commands/set/index.ts
@@ -3,11 +3,12 @@ import { CommandWithSubcommands } from "../command";
 import SetAboveRoleCommand from "./set.ar";
 import SetListCommand from "./set.list";
 import SetRoleDefaultColourCommand from "./set.rdc";
+import SetLogChannelCommand from "./set.lc";
 
 const SetCommand: CommandWithSubcommands = {
 	data: new SlashCommandBuilder().setName('set')
 		.setDescription('Set server settings. Requires moderator permissions.'),
-	subcommands: [ SetAboveRoleCommand, SetRoleDefaultColourCommand, SetListCommand ],
+	subcommands: [ SetAboveRoleCommand, SetRoleDefaultColourCommand, SetListCommand, SetLogChannelCommand ],
 };
 
 export default SetCommand;
